a3936853395c20aaa9f513231dffe50f2ddc3bc9
Nicasiomarques <nicasiomarques18@gmail.com>
2023-12-25 12:43:20 +0100

    chore: replace deprecated props and or by nullish coalescing

--- src/components/add-task/index.tsx
+++ src/components/add-task/index.tsx
@@ -1,8 +1,8 @@
-import { KeyboardEvent, useState } from 'react';
+import { KeyboardEvent, useState } from "react";
 
-import { randomHash, enterPressed } from '../../helpers';
-import { TodoProps, useTodo, useToggle } from '../../hooks';
-import { PlaceholderMode, ColorSelector } from '..';
+import { randomHash, enterPressed } from "../../helpers";
+import { TodoProps, useTodo, useToggle } from "../../hooks";
+import { PlaceholderMode, ColorSelector } from "..";
 
 export const AddTask = () => {
   const { onAdd } = useTodo();
@@ -11,33 +11,32 @@ export const AddTask = () => {
 
   const makeTodo = (todo: Partial<TodoProps>): TodoProps => ({
     id: randomHash(),
-    text: todo.text || 'not filled',
-    color: todo.color || 'blue',
+    text: todo.text ?? "not filled",
+    color: todo.color ?? "blue",
     completed: false,
   });
 
   const handleEnter = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (enterPressed(event)) {
+    if (enterPressed(event) && todo.text) {
       onAdd(makeTodo(todo));
-      setTodo((prev) => ({ ...prev, text: '' }));
-      toggle();
+      setTodo(prev => ({ ...prev, text: "" }));
     }
   };
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: "flex" }}>
       {!isEditing ? (
         <PlaceholderMode handleMode={toggle} />
       ) : (
-        <div className="add-task__wrapper">
+        <div className='add-task__wrapper'>
           <ColorSelector onSelectColor={setTodo} />
           <input
-            className="add-task__input"
-            placeholder="placeholder"
-            type="text"
+            className='add-task__input'
+            placeholder='placeholder'
+            type='text'
             value={todo.text}
-            onChange={(e) => {
-              setTodo((prev) => ({ ...prev, text: e.target.value }));
+            onChange={e => {
+              setTodo(prev => ({ ...prev, text: e.target.value }));
             }}
             onKeyPress={handleEnter}
           />
